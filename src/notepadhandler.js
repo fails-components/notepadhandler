9b6f51cb7515132adae3fa3cd2b4ee396a223e1b
Marten Richter <marten.richter@tu-berlin.de>
2021-10-31 12:44:38 +0100

    Add defaults to get Presentationinfo

--- src/notepadhandler.js
+++ src/notepadhandler.js
@@ -1413,9 +1413,9 @@ export class NoteScreenConnection {
       )
       lectprop = await lectprop
       return {
-        casttoscreens: lectprop[0],
-        backgroundbw: lectprop[1],
-        showscreennumber: lectprop[2]
+        casttoscreens: lectprop[0] !== null ? lectprop[0] : 'false',
+        backgroundbw: lectprop[1] !== null ? lectprop[1] : 'true',
+        showscreennumber: lectprop[2] !== null ? lectprop[2] : 'false'
       }
     } catch (error) {
       console.log('getPresentationinfo', error)
